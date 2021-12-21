9943e0b99b30d26000bc140a8825f41e97ea02c1
Vinicius Rocha <vinicius.rocha@shopify.com>
2021-12-21 12:32:29 -0500

    Remove SPIN files

--- install.sh
+++ install.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-if [ $SPIN ]; then
-    # Install packages
-    sudo apt install -y autoconf coreutils parallel zip bear
+# Add git aliases
+git config --global alias.co checkout 
+git config --global alias.sw switch
+git config --global commit.gpgsign true
 
-    # Add git aliases
-    git config --global alias.co checkout 
-    git config --global alias.sw switch
-    git config --global commit.gpgsign true
-
-    # Link dotfiles
-    #ln -sf ~/dotfiles/zshrc ~/.zshrc
-    ln -sf ~/dotfiles/vimrc ~/.vimrc
-    ln -sf ~/dotfiles/gemrc ~/.gemrc
-fi
+# Link dotfiles
+ln -sf ~/dotfiles/vimrc ~/.vimrc
+ln -sf ~/dotfiles/gemrc ~/.gemrc
